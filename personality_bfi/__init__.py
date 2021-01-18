d38a6d014048a3cd1f438efb86799fca703e5fd2
Paul Dulaud <paul.dulaud@utt.fr>
2021-01-18 14:39:14 +0100

    Modify __init__.py
    Modified __init__.py to stick to the Flask Application Factory layout.

--- personality_bfi/__init__.py
+++ personality_bfi/__init__.py
@@ -1,7 +1,34 @@
 __version__ = "0.1.0"
 
+
+import os
+
 from flask import Flask
 
-app = Flask(__name__)
 
-import personality_bfi.views
+def create_app(test_config=None):
+
+    # create and configure the WSGI application object
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        # DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        # app.config.from_object("config")
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from . import views
+
+    return app
