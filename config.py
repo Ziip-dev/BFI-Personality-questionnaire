f01885d270460220a093eeac6e09a9ee9a9450f4
Paul Dulaud <paul.dulaud@utt.fr>
2021-01-19 11:37:29 +0100

    Modify config.py to use configuration classes.

--- config.py
+++ config.py
@@ -1,34 +1,44 @@
-# Statement for enabling the development environment
-# DEBUG = True
-
-
-# Define the application dierctory
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-
-# Define the database - we are working with
-# SQLite for this example
-# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
-# DATABASE_CONNECT_OPTIONS = {}
-
-
 # Application threads. A common general assumption is
 # using 2 per available processor cores - to handle
 # incoming requests using one and performing background
 # operations using the other.
 # THREADS_PER_PAGE = 2
 
-
 # Enable protection agains *Cross-site Request Forgery (CSRF)*
 # CSRF_ENABLED     = True
-
 
 # Use a secure, unique and absolutely secret key for
 # signing the data.
 # CSRF_SESSION_KEY = "secret"
 
 
-# Secret key for signing cookies
-# SECRET_KEY = "secret"
+"""Flask configuration."""
+from os import environ, path
+from dotenv import load_dotenv
+
+# Define the application dierctory
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, ".env"))
+
+
+class Config:
+    """Base config."""
+
+    SECRET_KEY = environ.get("SECRET_KEY")
+    SESSION_COOKIE_NAME = environ.get("SESSION_COOKIE_NAME")
+    STATIC_FOLDER = "static"
+    TEMPLATES_FOLDER = "templates"
+
+
+class ProdConfig(Config):
+    FLASK_ENV = "production"
+    DEBUG = False
+    TESTING = False
+    # SERVER_NAME = If you intend your app to be reachable on a custom domain, we specify the app's domain name here.
+
+
+class DevConfig(Config):
+    FLASK_ENV = "development"
+    DEBUG = True
+    TESTING = True
+    # SERVER_NAME = If you intend your app to be reachable on a custom domain, we specify the app's domain name here.
--- personality_bfi/__init__.py
+++ personality_bfi/__init__.py
@@ -9,47 +9,21 @@ from flask import Flask
 # wtf = WTForm()
 
 
-# def create_app(test_config=None):
-
-#     # create and configure the WSGI application object
-#     app = Flask(__name__, instance_relative_config=True)
-#     app.config.from_mapping(
-#         SECRET_KEY="dev",
-#         # DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
-#     )
-
-#     if test_config is None:
-#         # load the instance config, if it exists, when not testing
-#         app.config.from_pyfile("config.py", silent=True)
-#     else:
-#         # load the test config if passed in
-#         app.config.from_mapping(test_config)
-
-#     # ensure the instance folder exists
-#     try:
-#         os.makedirs(app.instance_path)
-#     except OSError:
-#         pass
-
-#     from . import views
-
-#     return app
-
-
 def create_app():
     """Initialize the core application."""
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object("config.Config")
+    app.config.from_object("config.DevConfig")
 
     # Initialize Plugins (if necessary)
     # wtf.init_app(app)
 
     with app.app_context():
-        # Include all Routes
-        from . import routes
+        # Import parts of the application
+        from .home import routes
+        from .bfi_form import routes
 
         # Register Blueprints
-        # app.register_blueprint(auth.auth_bp)
-        # app.register_blueprint(admin.admin_bp)
+        app.register_blueprint(home.home_bp)
+        app.register_blueprint(bfi_form.form_bp)
 
         return app
