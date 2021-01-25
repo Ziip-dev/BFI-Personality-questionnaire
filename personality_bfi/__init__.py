48e10651a60599bb92f2bdd961f0b639afdb5f1c
Paul Dulaud <paul.dulaud@utt.fr>
2021-01-25 19:14:21 +0100

    Modify __init__.py and add assets.py
    Changes for incorporating assets management via flask-assets

--- personality_bfi/__init__.py
+++ personality_bfi/__init__.py
@@ -1,16 +1,19 @@
 __version__ = "0.1.0"
 
-
+"""Initialize app."""
 import os
 from flask import Flask
+from flask_assets import Environment
+from .assets import compile_assets
 
+assets = Environment()
 
 # Globally accessible libraries
 # wtf = WTForm()
 
 
 def create_app():
-    """Initialize core application."""
+    """Construct core application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object("config.DevConfig")
 
