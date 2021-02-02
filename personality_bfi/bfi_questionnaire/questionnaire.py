ba1a2c86c44be38a16012682fd0bc451724d14ad
Paul Dulaud <paul.dulaud@utt.fr>
2021-02-02 22:04:09 +0100

    Modify questionnaire.py with proper RadioField.
    Proper RadioField used and choices adapted to BFI questionnaire

--- personality_bfi/bfi_questionnaire/questionnaire.py
+++ personality_bfi/bfi_questionnaire/questionnaire.py
@@ -7,13 +7,13 @@ from wtforms.validators import DataRequired
 
 class BfiQuestionnaire(FlaskForm):
     answer = RadioField(
-        "Label", choices=[("val1", "desc1"), ("val2", "desc2"), ("val3", "desc3")]
+        choices=[
+            (1, "désapprouve fortement"),
+            (2, "désapprouve un peu"),
+            (3, "n'approuve ni ne désapprouve"),
+            (4, "approuve un peu"),
+            (5, "approuve fortement"),
+        ],
+        validators=[DataRequired()],
     )
     submit = SubmitField("Question Suivante")
-
-
-# 1 - désapprouve fortement
-# 2 - désapprouve un peu
-# 3 - n'approuve ni ne désapprouve
-# 4 - approuve un peu
-# 5 - approuve fortement
