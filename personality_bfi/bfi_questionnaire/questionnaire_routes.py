56a3093015e0a59288383dadf90595a96378467c
Paul Dulaud <paul.dulaud@utt.fr>
2021-02-08 21:34:16 +0100

    Add instructions route and template.

--- personality_bfi/bfi_questionnaire/questionnaire_routes.py
+++ personality_bfi/bfi_questionnaire/questionnaire_routes.py
@@ -19,48 +19,7 @@ questionnaire_bp = Blueprint(
 # Blueprint routes
 @questionnaire_bp.route("/bfi/", methods=["GET"])
 def bfi_welcome():
-    title = """
-    Big Five Inventory Français
-    """
-    explanation = """
-    The Big five Inventory is a personality questionnaire that relies on trait theory.
-    """
-
-    return """
-    <h1>{title}</h1>
-    {explanation}
-    <br>
-    <br>
-    <a href={{ request.path }}instructions>Click here to take the test!</a>
-    """.format(
-        title=title, explanation=explanation
-    )
-
-
-@questionnaire_bp.route("/bfi/instructions/")
-def bfi_instructions():
-    instructions = """
-    Vous allez trouver un certain nombre de qualificatifs qui peuvent ou non s'appliquer à vous.<br>
-    Par exemple, acceptez-vous d'être quelqu'un qui aime passer du temps avec les autres ?<br>
-    Ecrivez le chiffre indiquant combien vous approuvez ou désapprouvez l'affirmation :
-    """
-
-    # reponses_possibles
-    answers_allowed = """
-    1 - désapprouve fortement<br>
-    2 - désapprouve un peu<br>
-    3 - n'approuve ni ne désapprouve<br>
-    4 - approuve un peu<br>
-    5 - approuve fortement<br>
-    """
-
-    return (
-        instructions
-        + "<br><br>"
-        + answers_allowed
-        + "<br><br>"
-        + "<a href={{ url_for('questionnaire_bp.bfi_questionnaire', question_number=1) }}>C'est parti !"
-    )
+    return render_template("instructions.jinja2")
 
 
 @questionnaire_bp.route(
