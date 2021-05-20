0a85222d53711726855d451778f8a6f4335a5d0f
Paul Dulaud <paul.dulaud@utt.fr>
2021-05-20 17:39:43 +0200

    Templates update with Tailwind CSS

--- start_container.sh
+++ start_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 app="personality.bfi"
 docker build -t ${app} .
-docker run --name=${app} --rm -d -p 80:5001 \
+docker run --name=${app} --rm -d -p 5001:5001 \
     --env-file=.env \
     ${app}
