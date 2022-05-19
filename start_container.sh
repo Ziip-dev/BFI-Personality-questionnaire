f352d5b2ce00ab8c6411365bd2b0118ddf7214ba
Ziip <paul.dulaud@utt.fr>
2022-05-19 12:52:25 +0200

    Final tweaks, deployment ready

--- start_container.sh
+++ start_container.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-app="personality.bfi"
-docker build -t ${app} .
+app="bfi"
+
+# build latest image version
+docker build --pull --rm -t ${app} "."
+
+# run container
 docker run --name=${app} --rm -d -p 80:5001 \
     --env-file=.env \
     ${app}
